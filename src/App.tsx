632248b581d770c550cde67f5a1024dfd94318fb
LiuChunrui <LiuChunrui@DESKTOP-DS8TL2R>
2022-08-15 23:46:59 +0800

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useCallback } from 'react'
-import Tableau, { TableauProps } from './Tableau';
+import React, { useCallback, FC } from 'react'
+import Tableau, { TableauProps } from './Tableau'
 
-function App() {
+const App: FC = () => {
   const onMarkSelected = useCallback((event) => {
-    console.log(event);
-  }, []);
+    console.log(event)
+  }, [])
+  const onFilterChanged = useCallback((event) => {
+    console.log(event)
+  })
   const dbProps: TableauProps = {
     url: 'https://public.tableau.com/views/RegionalSampleWorkbook/Storms',
     onMarkSelected,
+    onFilterChanged,
     autoResize: true
-  };
+  }
   return (
     <div className="App">
       <Tableau {...dbProps} />
--- src/Tableau.tsx
+++ src/Tableau.tsx
@@ -1,73 +1,72 @@
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC, useEffect, useRef } from 'react'
 import {
-	Viz, ErrorCode, TableauEventName, VizCreateOptions,
-	ApiToolbarPosition, FilterEvent, ParameterEvent, CustomMarkContextMenu,
-	CustomViewEvent, StoryPointSwitchEvent, TabSwitchEvent, ToolbarStateEvent,
-	UrlActionEvent, VizResizeEvent
-} from 'tableau-js';
+  Viz, TableauEventName, VizCreateOptions,
+  FilterEvent, ParameterEvent, CustomMarkContextMenu,
+  CustomViewEvent, StoryPointSwitchEvent, TabSwitchEvent, ToolbarStateEvent,
+  UrlActionEvent, VizResizeEvent
+} from 'tableau-js'
 
-const events : [string, TableauEventName][] = [
-	['onMarkSelected', TableauEventName.MARKS_SELECTION],
-	['onFilterChanged', TableauEventName.FILTER_CHANGE],
-	['onParamValueChanged', TableauEventName.PARAMETER_VALUE_CHANGE],
-	['onContextMenuSelected', TableauEventName.CUSTOM_MARK_CONTEXT_MENU],
-	['onCustomViewLoaded', TableauEventName.CUSTOM_VIEW_LOAD],
-	['onCustomViewRemoved', TableauEventName.CUSTOM_VIEW_REMOVE],
-	['onCustomViewSaved', TableauEventName.CUSTOM_VIEW_SAVE],
-	['onCustomViewSetDefault', TableauEventName.CUSTOM_VIEW_SET_DEFAULT],
-	['onStoryPointSwitch', TableauEventName.STORY_POINT_SWITCH],
-	['onTabSwitch', TableauEventName.TAB_SWITCH],
-	['onToolbarStateChanged', TableauEventName.TOOLBAR_STATE_CHANGE],
-	['onUrlAction', TableauEventName.URL_ACTION],
-	['onVizResized', TableauEventName.VIZ_RESIZE]
-];
+const events: Array<[string, TableauEventName]> = [
+  ['onMarkSelected', TableauEventName.MARKS_SELECTION],
+  ['onFilterChanged', TableauEventName.FILTER_CHANGE],
+  ['onParamValueChanged', TableauEventName.PARAMETER_VALUE_CHANGE],
+  ['onContextMenuSelected', TableauEventName.CUSTOM_MARK_CONTEXT_MENU],
+  ['onCustomViewLoaded', TableauEventName.CUSTOM_VIEW_LOAD],
+  ['onCustomViewRemoved', TableauEventName.CUSTOM_VIEW_REMOVE],
+  ['onCustomViewSaved', TableauEventName.CUSTOM_VIEW_SAVE],
+  ['onCustomViewSetDefault', TableauEventName.CUSTOM_VIEW_SET_DEFAULT],
+  ['onStoryPointSwitch', TableauEventName.STORY_POINT_SWITCH],
+  ['onTabSwitch', TableauEventName.TAB_SWITCH],
+  ['onToolbarStateChanged', TableauEventName.TOOLBAR_STATE_CHANGE],
+  ['onUrlAction', TableauEventName.URL_ACTION],
+  ['onVizResized', TableauEventName.VIZ_RESIZE]
+]
 
 export interface TableauProps extends VizCreateOptions {
-	url: string;
-	autoResize?: boolean;
-	onMarkSelected?: (event: MarksEvent) => void;
-	onFilterChanged?: (event: FilterEvent) => void;
-	onParamValueChanged?: (event: ParameterEvent) => void;
-	onContextMenuSelected?: (event: CustomMarkContextMenu) => void;
-	onCustomViewLoaded?: (event: CustomViewEvent) => void;
-	onCustomViewRemoved?: (event: CustomViewEvent) => void;
-	onCustomViewSaved?: (event: CustomViewEvent) => void;
-	onCustomViewSetDefault?: (event: CustomViewEvent) => void;
-	onStoryPointSwitch?: (event: StoryPointSwitchEvent) => void;
-	onTabSwitch?: (event: TabSwitchEvent) => void;
-	onToolbarStateChanged?: (event: ToolbarStateEvent) => void;
-	onUrlAction?: (event: UrlActionEvent) => void;
-	onVizResized?: (event: VizResizeEvent) => void;
+  url: string
+  autoResize?: boolean
+  onMarkSelected?: (event: MarksEvent) => void
+  onFilterChanged?: (event: FilterEvent) => void
+  onParamValueChanged?: (event: ParameterEvent) => void
+  onContextMenuSelected?: (event: CustomMarkContextMenu) => void
+  onCustomViewLoaded?: (event: CustomViewEvent) => void
+  onCustomViewRemoved?: (event: CustomViewEvent) => void
+  onCustomViewSaved?: (event: CustomViewEvent) => void
+  onCustomViewSetDefault?: (event: CustomViewEvent) => void
+  onStoryPointSwitch?: (event: StoryPointSwitchEvent) => void
+  onTabSwitch?: (event: TabSwitchEvent) => void
+  onToolbarStateChanged?: (event: ToolbarStateEvent) => void
+  onUrlAction?: (event: UrlActionEvent) => void
+  onVizResized?: (event: VizResizeEvent) => void
 }
 
 const Tableau: FC<TableauProps> = (props) => {
-	const div = useRef<HTMLDivElement>(null);
-	const { url, height, width, autoResize, onFirstVizSizeKnown, ...rest } = props;
-	Viz
-	useEffect(() => {
-		const option: VizCreateOptions = {
-			onFirstInteractive: (e) => {
-				const viz = e.getViz();
-				for (const [handlerName, eventName] of events) {
-					if (props[handlerName] instanceof Function) {
-						viz.addEventListener(eventName, props[handlerName]);
-					}
-				}
-			},
-			onFirstVizSizeKnown,
-			...rest
-		};
-		const viz = new Viz(div.current, url, option);
-		return () => {
-			for (const [handlerName, eventName] of events) {
-				if (props[handlerName] instanceof Function) {
-					viz.removeEventListener(eventName, props[handlerName]);
-				}
-			}
-			viz.dispose();
-		};
-	}, [props])
-	return <div ref={div} />;
+  const div = useRef<HTMLDivElement>(null)
+  const { url, height, width, autoResize, onFirstVizSizeKnown, ...rest } = props
+  useEffect(() => {
+    const option: VizCreateOptions = {
+      onFirstInteractive: (e) => {
+        const viz = e.getViz()
+        for (const [handlerName, eventName] of events) {
+          if (props[handlerName] instanceof Function) {
+            viz.addEventListener(eventName, props[handlerName])
+          }
+        }
+      },
+      onFirstVizSizeKnown,
+      ...rest
+    }
+    const viz = new Viz(div.current, url, option)
+    return () => {
+      for (const [handlerName, eventName] of events) {
+        if (props[handlerName] instanceof Function) {
+          viz.removeEventListener(eventName, props[handlerName])
+        }
+      }
+      viz.dispose()
+    }
+  }, [props])
+  return <div ref={div} />
 }
 
-export default Tableau;
+export default Tableau
